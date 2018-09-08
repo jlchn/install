4bad97df2467bddb2cc616a0bd307e4fa31a370c
jlchn <jianglichn@163.com>
2018-09-08 21:26:11 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -8,5 +8,5 @@ smbclient -L 192.168.0.103 -N
 
 # create a dir for mounting the remote dir
 sudo mkdir /mnt/samba 
-
-sudo mount -t cifs //192.168.0.103/Public /mnt/samba/ -o guest # -t cifs means using SMB file System, -o guest means using anounymous account
+# -t cifs means using SMB file System, -o guest means using anounymous account
+sudo mount -t cifs //192.168.0.103/Public /mnt/samba/ -o guest 

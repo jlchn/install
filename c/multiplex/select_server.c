1ca353be2e81078b1414bdd9bf18ebc2d2a61add
jiangli <jianglichn@163.com>
2019-10-11 15:04:31 +0800

    update select server

--- c/multiplex/select_server.c
+++ c/multiplex/select_server.c
@@ -10,17 +10,52 @@
 #include <unistd.h> 
 #include <sys/select.h>
 #define PORT 5000 
-#define MAXLINE 1024 
+#define MAXLINE 10
+
+int read_from_client (int filedes)
+{
+  char buffer[MAXLINE];
+  int nbytes;
+
+  bzero(buffer, sizeof(buffer)); 
+  nbytes = read (filedes, buffer, MAXLINE);
+  if (nbytes < 0)
+  {
+    perror ("error on read");
+    exit (1);
+  }
+  else if (nbytes == 0)
+  {
+    fputs("eof", stdout);
+    return -1; /* End-of-file. */
+  }
+  else
+  {
+    // fputs(buffer, stdout);
+    fprintf (stdout, "Server: got message: `%s'\n", buffer);
+    write(filedes, (const char*)buffer, nbytes); //nbytes instead of sizeof(buffer)
+    read_from_client(filedes);
+    return 0;
+  }
+}
+
+int max(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
 
 int main() 
 { 
-    int listenfd, connfd, nready; 
+    int listenfd, connfd = -1, nready; 
     char buffer[MAXLINE]; 
     pid_t childpid; 
     fd_set rset; 
     ssize_t n; 
     socklen_t len; 
-    const int on = 1; 
     struct sockaddr_in cliaddr, servaddr; 
     void sig_chld(int); 
   
@@ -45,15 +80,11 @@ int main()
             connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &len); 
             if ((childpid = fork()) == 0) { 
                 close(listenfd); 
-                bzero(buffer, sizeof(buffer)); 
-                printf("Message from client: "); 
-                read(connfd, buffer, sizeof(buffer)); 
-                puts(buffer); 
-                write(connfd, (const char*)buffer, sizeof(buffer)); 
-                close(connfd); 
+                read_from_client(connfd);
+                close(connfd);
                 exit(0); 
             } 
-            close(connfd); 
+        close(connfd); // close in the parent process
         } 
     } 
 } 
